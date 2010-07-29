8425288f42fbe37ca72530335afd51310d5e2eb7
Fernando Espinosa <fer@ferdev.com>
2010-07-29 17:14:15 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetsWaiting::Application.config.secret_token = '153f752ae0dc19dc5773f0b83fce3883d80105c7d4421dcc4be4aa99b61185e11e949976470e97c7de5b932ddbf68766200aa359c4cc8c869867654c3a51d345'
