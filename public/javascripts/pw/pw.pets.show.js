3346ffcecac97702fe4ac570bbdce133485b45ad
Fernando Espinosa <fer@ferdev.com>
2011-02-12 20:27:42 +0100

    Adds google maps in pet's page

--- public/javascripts/pw/pw.pets.show.js
+++ public/javascripts/pw/pw.pets.show.js
@@ -2,6 +2,29 @@ $.extend($.pw.pets, {
   show: {
     init: function(){
       $('div.pet div.photos').slider();
+
+      if (PET_ADDRESS) {
+        var
+          map_div       = $('#content div.pet .description_and_location .location .map'),
+          map_container = $('<div/>').addClass('container').appendTo(map_div).get(0);
+
+        map_div.find('img').remove();
+
+        var
+          geocoder = new google.maps.Geocoder(),
+          myOptions = {
+            disableDefaultUI: true,
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+          },
+          map = new google.maps.Map(map_container, myOptions);
+
+        geocoder.geocode( { 'address': PET_ADDRESS}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+          };
+        });
+      };
     }
   }
 });
